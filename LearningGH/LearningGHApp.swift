954b52f0e3af19c87d50d8289cb1d60257799115
Leo Cadang <leo.cadang@gmail.com>
2021-05-29 19:57:33 -0700

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Leonilo Cadang on 5/29/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
